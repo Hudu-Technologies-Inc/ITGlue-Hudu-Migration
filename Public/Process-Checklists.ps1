24e96e87430d005569af0b6acf58415f2773611e
masonville17 <anoniboni9876@gmail.com>
2026-06-01 13:22:12 -0400

    hardening null-coalescence to use nullorempty on ITGAPIENDPOINT var

--- Public/Process-Checklists.ps1
+++ Public/Process-Checklists.ps1
@@ -8,9 +8,10 @@ foreach ($u in $huduUsers) {$key = "$($u.first_name) $($u.last_name)".ToLower();
 
 if (-not (Get-Command -Name Get-ITGlueCheckLists -ErrorAction SilentlyContinue)) { . "$($(get-childitem -path "." -Recurse -file "Get-Checklists.ps1" | Select-Object -first 1).fullname)" }
 if (-not (Get-Command -Name Get-ITGlueJWTAuth -ErrorAction SilentlyContinue)) { . "$($(get-childitem -path "." -Recurse -file "JWT-Auth.ps1" | Select-Object -first 1).fullname)" }
-$ITGAPIEndpoint = $settings.ITGAPIEndpoint ?? 
-    $(Select-ObjectFromList -objects @("https://api.itglue.com", "https://api.eu.itglue.com", "https://api.au.itglue.com") -message "Select ITGlue API Endpoint for your instance/region")
-
+$ITGAPIEndpoint = $ITGAPIEndpoint ?? $settings.ITGAPIEndpoint
+if ([string]::isNullOrWhiteSpace($ITGAPIEndpoint)){ 
+    $ITGAPIEndpoint = $(Select-ObjectFromList -objects @("https://api.itglue.com", "https://api.eu.itglue.com", "https://api.au.itglue.com") -message "Select ITGlue API Endpoint for your instance/region")
+}
 $ITGlueJWT = $ITGlueJWT ?? (Read-Host "Please enter your ITGlue JWT as retrieved from browser.")
 $ITGlueJWT = Get-ITGlueJWTAuth -ITglueJWT $ITglueJWT -ITGBaseURI $ITGAPIEndpoint
 
