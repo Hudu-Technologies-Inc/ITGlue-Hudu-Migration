2a8a8b8fc379e2fcb86c5cd5ef2dbc1492a4fa2a
masonville17 <anoniboni9876@gmail.com>
2026-08-27 11:48:31 -0400

    removing one-off skipping of M$-duped layout

--- ITGlue-Hudu-Migration.ps1
+++ ITGlue-Hudu-Migration.ps1
@@ -1277,7 +1277,7 @@ if ($ResumeFound -eq $true -and (Test-Path "$MigrationLogs\AssetLayouts.json")) 
     # Match to existing layouts
     $MatchedLayouts = foreach ($ITGLayout in $FlexLayouts) {
         if ($skipIntegratorLayouts -and $true -eq $skipIntegratorLayouts){
-            if ("$($ITGLayout.attributes.name)" -ilike "*(auto)*"  -or "$($ITGLayout.attributes.name)" -ilike "*9fd7*" -or "$($ITGLayout.attributes.name)" -ilike "*b857*" -or  "$($ITGLayout.attributes.name)" -ilike "*(liongard)*" -or "$($ITGLayout.attributes.name)" -ilike "*datto*rmm*" -or "$($ITGLayout.attributes.name)" -ilike "*connectwise*"  -or "$($ITGLayout.attributes.name)" -ilike "*autotask*" ){
+            if ("$($ITGLayout.attributes.name)" -ilike "*(auto)*" -or  "$($ITGLayout.attributes.name)" -ilike "*(liongard)*" -or "$($ITGLayout.attributes.name)" -ilike "*datto*rmm*" -or "$($ITGLayout.attributes.name)" -ilike "*connectwise*"  -or "$($ITGLayout.attributes.name)" -ilike "*autotask*" ){
                 Write-warning "Skipping Integrator Layout $($ITGLayout.attributes.name)"
                 continue
             }
@@ -1416,7 +1416,7 @@ if ($ResumeFound -eq $true -and (Test-Path "$MigrationLogs\AssetLayouts.json")) 
 
         foreach ($UpdateLayout in $MatchedLayouts) {
             if ($skipIntegratorLayouts -and $true -eq $skipIntegratorLayouts){
-                if ("$($UpdateLayout.Name)" -ilike "*(auto)*" -or "$($UpdateLayout.Name)" -ilike "*b857*" -or "$($UpdateLayout.Name)" -ilike "*9fd7*" -or "$($UpdateLayout.Name)" -ilike "*(liongard)*" -or "$($UpdateLayout.Name)" -ilike "*datto*rmm*" -or "$($UpdateLayout.Name)" -ilike "*connectwise*"  -or "$($UpdateLayout.Name)" -ilike "*autotask*" ){ 
+                if ("$($UpdateLayout.Name)" -ilike "*(auto)*" -or "$($UpdateLayout.Name)" -ilike "*(liongard)*" -or "$($UpdateLayout.Name)" -ilike "*datto*rmm*" -or "$($UpdateLayout.Name)" -ilike "*connectwise*"  -or "$($UpdateLayout.Name)" -ilike "*autotask*" ){ 
                     Write-Host "Skipping Integrator Layout $($UpdateLayout.Name)" -ForegroundColor Yellow
                     continue
                 }
